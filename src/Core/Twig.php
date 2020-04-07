b55cc54afd8dfbe34feb25e3d40560ce532dbbef
Dugi97 <lazardugalic1@gmail.com>
2020-04-07 17:17:02 +0200

    core structure

---
+++ src/Core/Twig.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Core;
+
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * Class Twig
+ * @package App\Core
+ */
+class Twig
+{
+    /**
+     * The Render method will be used in order to render the template
+     *
+     * @param string $path The path of the template. For example ('default/index.html.twig')
+     * @param array $params The params which will be passed to the template
+     *
+     * @return void
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    static function render(string $path, array $params = [])
+    {
+        $loader = new FilesystemLoader(__DIR__ . '/../../templates/');
+        $twig = new Environment($loader, [
+            'debug' => true
+        ]);
+        $twig->addExtension(new DebugExtension());
+
+        echo $twig->render($path, $params);
+    }
+}
